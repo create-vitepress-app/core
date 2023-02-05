57be89856a78227ed044eb821707858aa9324b72
FLzyy <flzyy72@gmail.com>
2023-02-05 19:48:50 +0800

    use ts for build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -12,12 +12,6 @@ await writeFile(
   Buffer.from(JSON.stringify(object), "utf-8")
 );
 
-// Copy over the types
-await writeFile(
-  "./dist/index.d.ts",
-  await readFile("./src/types/index.d.ts", "utf-8")
-);
-
 // Copy the README file over.
 await copyFile("./README.md", "./dist/README.md");
 
